a9e42a47272faf8d2f6601422b8dec1a32cf7158
Harsh-singh-9326 <harshs9326@gmail.com>
2024-07-13 00:37:20 +0530

    add a new code

---
+++ src/forloop_learning.java
@@ -0,0 +1,16 @@
+//loops and iterable statement are used to  repeat a block of code
+//there are three loops for loops, while loops, do while loops in java
+//lets do for loops practise print 1 to 10 using for loops
+public class forloop_learning {
+    public static void main(String[] args) {
+int k = 10; // declare a variable
+        //for loops is used to run a block of code number of times
+        for (int i = 0; i <=k; i++) {
+            //initial expression =(int i = 0;): is declared variable it run only once in loops
+            //test expression = (i <=k;): it evaluate the condition and run until it become false
+            //update expression = (i++): it update the value of an initial expression
+            System.out.println("hello number of times " + i);
+        }
+        //for loops has done
+    }
+}
